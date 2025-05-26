215f33d1a6b469650de25b5f48d235a72031a136
Brian <596833+flizzer@users.noreply.github.com>
2025-05-26 17:01:55 -0400

    final updates to solar system lesson

--- src/main.js
+++ src/main.js
@@ -12,6 +12,12 @@ class App {
   #controls_ = null;
 
   #sun_ = null;
+  #earth_ = null;
+
+  #earthOrbit_ = null;
+  #moonOrbit_ = null;
+
+  #orbits_ = [];
 
   constructor() {
   }
@@ -48,19 +54,153 @@ class App {
     this.#scene_ = new THREE.Scene();
     this.#scene_.background = new THREE.Color(0x000000);
 
-    this.#CreateSolarSystem_();
+    //this.#CreateSolarSystem2_();
+
+    this.#CreateSolarSystem3_([
+      {
+        name: 'earth',
+        size: 5,
+        color: new THREE.Color(0x0000ff),
+        distance: 60,
+        speed: 1,
+        moons: [
+          {
+            name: 'moon',
+            size: 1,
+            color: new THREE.Color(0x888888),
+            distance: 8,
+            speed: 2
+          }
+        ], 
+      },
+      {
+        name: 'mars',
+        size: 4,
+        color: new THREE.Color(0xff0000),
+        distance: 100,
+        speed: .5,
+        moons: [
+          {
+            name: 'phobos',
+            size: 1,
+            color: new THREE.Color(0x888888),
+            distance: 7,
+            speed: 3
+          },
+          {
+            name: 'delmos',
+            size: 1,
+            color: new THREE.Color(0x888888),
+            distance: 11,
+            speed: 4
+          }
+        ], 
+      }
+    ]);
   }
 
-  #CreateSolarSystem_() {
+  #CreateSolarSystem3_(planets) {
+
+    const sunGeo = new THREE.SphereGeometry(40, 32, 32);
+    const sunMat = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+    const sun = new THREE.Mesh(sunGeo, sunMat);
+    //sun.add(this.#earthOrbit_);
+    this.#scene_.add(sun);
+
+    for (let i = 0; i < planets.length; i++)
+    {
+      const planetdata = planets[i];
+      const planetGeo = new THREE.SphereGeometry(planetdata.size, 32, 32);
+      const planetMat = new THREE.MeshBasicMaterial({color: planetdata.color});
+      const planet = new THREE.Mesh(planetGeo, planetMat);
+      planet.position.set(planetdata.distance, 0, 0);
+
+      const planetOrbit = new THREE.Group();
+      planetOrbit.add(planet);
+
+      for (let j = 0; j < planetdata.moons.length; j++)
+      {
+        const moondata = planetdata.moons[j];
+        const moonGeo = new THREE.SphereGeometry(moondata.size, 0, 0);
+        const moonMat = new THREE.MeshBasicMaterial({color: moondata.color});
+        const moon = new THREE.Mesh(moonGeo, moonMat);
+        moon.position.set(moondata.distance, 0, 0);
+        const moonOrbit = new THREE.Group();
+        moonOrbit.add(moon);
+        planet.add(moonOrbit);
+        this.#orbits_.push({target: moonOrbit, speed: moondata.speed});
+      }
+
+      this.#orbits_.push({target: planetOrbit, speed: planetdata.speed});
+      sun.add(planetOrbit);
+    }
+
+    // const moonGeo = new THREE.SphereGeometry(1, 32, 32)
+    // const moonMat = new THREE.MeshBasicMaterial({ color: 0x888888 });
+    // const moon = new THREE.Mesh(moonGeo, moonMat);
+    // moon.position.set(8, 0, 0)
+    
+    // this.#moonOrbit_ = new THREE.Group();
+    // this.#moonOrbit_.add(moon);
+    // this.#moonOrbit_.rotateX(.5);
+
+    // const earthGeo = new THREE.SphereGeometry(5, 32, 32);
+    // const earthMat = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+    // this.#earth_ = new THREE.Mesh(earthGeo, earthMat);
+    // this.#earth_.position.set(60, 0, 0);
+    // this.#earth_.add(this.#moonOrbit_);
+
+    // this.#earthOrbit_ = new THREE.Group();
+    // this.#earthOrbit_.add(this.#earth_);
+
+    // const sunGeo = new THREE.SphereGeometry(40, 32, 32);
+    // const sunMat = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+    // this.#sun_ = new THREE.Mesh(sunGeo, sunMat);
+    // this.#sun_.add(this.#earthOrbit_);
+    // this.#scene_.add(this.#sun_);
+  }
+
+  #CreateSolarSystem2_() {
+    const moonGeo = new THREE.SphereGeometry(1, 32, 32)
+    const moonMat = new THREE.MeshBasicMaterial({ color: 0x888888 });
+    const moon = new THREE.Mesh(moonGeo, moonMat);
+    moon.position.set(8, 0, 0)
+    
+    this.#moonOrbit_ = new THREE.Group();
+    this.#moonOrbit_.add(moon);
+    this.#moonOrbit_.rotateX(.5);
+
     const earthGeo = new THREE.SphereGeometry(5, 32, 32);
     const earthMat = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-    const earth = new THREE.Mesh(earthGeo, earthMat);
-    earth.position.set(60,0,0);
+    this.#earth_ = new THREE.Mesh(earthGeo, earthMat);
+    this.#earth_.position.set(60, 0, 0);
+    this.#earth_.add(this.#moonOrbit_);
+
+    this.#earthOrbit_ = new THREE.Group();
+    this.#earthOrbit_.add(this.#earth_);
 
     const sunGeo = new THREE.SphereGeometry(40, 32, 32);
     const sunMat = new THREE.MeshBasicMaterial({ color: 0xffff00 });
     this.#sun_ = new THREE.Mesh(sunGeo, sunMat);
-    this.#sun_.add(earth);
+    this.#sun_.add(this.#earthOrbit_);
+    this.#scene_.add(this.#sun_);
+  }
+
+  #CreateSolarSystem1_() {
+    const moonGeo = new THREE.SphereGeometry(1, 32, 32)
+    const moonMat = new THREE.MeshBasicMaterial({ color: 0x888888 });
+    const moon = new THREE.Mesh(moonGeo, moonMat);
+    moon.position.set(8, 0, 0)
+    const earthGeo = new THREE.SphereGeometry(5, 32, 32);
+    const earthMat = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+    this.#earth_ = new THREE.Mesh(earthGeo, earthMat);
+    this.#earth_.position.set(60, 0, 0);
+    this.#earth_.add(moon);
+
+    const sunGeo = new THREE.SphereGeometry(40, 32, 32);
+    const sunMat = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+    this.#sun_ = new THREE.Mesh(sunGeo, sunMat);
+    this.#sun_.add(this.#earth_);
     this.#scene_.add(this.#sun_);
   }
 
@@ -92,8 +232,17 @@ class App {
   }
 
   #step_(timeElapsed) {
-    this.#controls_.update(timeElapsed);
-    this.#sun_.rotateY(timeElapsed);
+    for (let i = 0; i < this.#orbits_.length; i++)
+    {
+      const orbit = this.#orbits_[i];
+      orbit.target.rotateY(timeElapsed * orbit.speed)  
+    }
+    
+    
+    //this.#controls_.update(timeElapsed);
+    //this.#sun_.rotateY(timeElapsed);
+    // this.#earthOrbit_.rotateY(timeElapsed);
+    // this.#moonOrbit_.rotateY(timeElapsed * 2);
   }
 }
 
